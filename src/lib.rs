4b143a97b0d7f4b94b2cd069980de54612329362
Tomáš Diblík <dibla.tomas@post.cz>
2024-06-04 19:43:27 +0200

    [Docs] Comments to improve DX
    + some comments explaining harder-to-understand parts of the code

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 use serde::Serialize;
 
+// Just extending the `anyhow::Error`
 #[derive(Debug)]
 pub struct TACommandError(pub anyhow::Error);
+impl std::error::Error for TACommandError {}
 impl std::fmt::Display for TACommandError {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         #[cfg(any(debug_assertions, feature = "show_errs_in_release"))]
@@ -15,7 +17,9 @@ impl std::fmt::Display for TACommandError {
         }
     }
 }
-impl std::error::Error for TACommandError {}
+
+// Every "renspose" from a tauri command needs to be serializeable into json with serde.
+// This is why we cannot use `anyhow` directly. This piece of code fixes that.
 impl Serialize for TACommandError {
     fn serialize<S>(&self, serializer: S) -> std::result::Result<S::Ok, S::Error>
     where
@@ -32,13 +36,27 @@ impl Serialize for TACommandError {
         }
     }
 }
+
+// Ability to convert between `anyhow::Error` and `TACommandError`
 impl From<anyhow::Error> for TACommandError {
     fn from(error: anyhow::Error) -> Self {
         Self(error)
     }
 }
 
+/// Use this as your command's return type.
+///
+/// Example usage:
+/// ```
+/// #[tauri::command]
+/// fn test() -> anyhow_tauri::TAResult<String> {
+///     Ok("No error thrown.".into())
+/// }
+/// ```
+///
+/// You can find more examples inside the library's repo at `/demo/src-tauri/src/main.rs`
 pub type TAResult<T> = std::result::Result<T, TACommandError>;
+
 pub trait IntoTAResult<T> {
     fn into_ta_result(self) -> TAResult<T>;
 }
@@ -47,17 +65,50 @@ impl<T, E> IntoTAResult<T> for std::result::Result<T, E>
 where
     E: Into<anyhow::Error>,
 {
+    /// Maps errors, which can be converted into `anyhow`'s error type, into `TACommandError` which can be returned from command call.
+    /// This is a "quality of life" improvement.
+    ///
+    /// Example usage:
+    /// ```
+    /// #[tauri::command]
+    /// fn test_into_ta_result() -> anyhow_tauri::TAResult<String> {
+    ///     function_that_succeeds().into_ta_result()
+    ///     // could also be written as:
+    ///     // Ok(function_that_succeeds()?)
+    /// }
+    /// ```
     fn into_ta_result(self) -> TAResult<T> {
         self.map_err(|e| TACommandError(e.into()))
     }
 }
 impl<T> IntoTAResult<T> for anyhow::Error {
+    /// Maps `anyhow`'s error type into `TACommandError` which can be returned from a command call.
+    /// This is a "quality of life" improvement.
+    ///
+    /// Example usage:
+    /// ```
+    /// #[tauri::command]
+    /// fn test_into_ta_result() -> anyhow_tauri::TAResult<String> {
+    ///     function_that_succeeds().into_ta_result()
+    ///     // could also be written as:
+    ///     // Ok(function_that_succeeds()?)
+    /// }
+    /// ```
     fn into_ta_result(self) -> TAResult<T> {
         Err(TACommandError(self))
     }
 }
 
 pub trait IntoEmptyTAResult<T> {
+    /// Usefull whenever you want to create `Result<(), TACommandError>` (or `TAResult<()>`)
+    ///
+    /// Example usage:
+    /// ```
+    /// #[tauri::command]
+    /// fn test_into_ta_empty_result() -> anyhow_tauri::TAResult<()> {
+    ///     anyhow::anyhow!("Showcase of the .into_ta_empty_result()").into_ta_empty_result()
+    /// }
+    /// ```
     fn into_ta_empty_result(self) -> TAResult<T>;
 }
 impl IntoEmptyTAResult<()> for anyhow::Error {
@@ -66,6 +117,15 @@ impl IntoEmptyTAResult<()> for anyhow::Error {
     }
 }
 
+/// Mirrors the `anyhow::bail!` implementation, but calls `.into_ta_result()` afterwards.
+///
+/// Example usage:
+/// ```
+/// #[tauri::command]
+/// fn test_bail() -> anyhow_tauri::TAResult<String> {
+///     anyhow_tauri::bail!("Showcase of the .bail!()")
+/// }
+/// ```
 #[macro_export]
 macro_rules! bail {
     ($msg:literal $(,)?) => {
@@ -79,6 +139,16 @@ macro_rules! bail {
     };
 }
 
+/// Mirrors the `anyhow::ensure!` implementation, but calls `.into_ta_result()` afterwards.
+///
+/// Example usage:
+/// ```
+/// #[tauri::command]
+/// fn test_ensure() -> anyhow_tauri::TAResult<String> {
+///     anyhow_tauri::ensure!(1 == 2); // this should throw
+///     Ok("this should never trigger".to_owned())
+/// }
+/// ```
 #[macro_export]
 macro_rules! ensure {
     ($cond:expr $(,)?) => {
