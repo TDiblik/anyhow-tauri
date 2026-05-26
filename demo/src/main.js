d11564030ea89e282cc21daefaf022fb93782f5c
Tomáš Diblík <dibla.tomas@post.cz>
2026-05-26 21:02:48 +0200

    [Chore] update demo to the latest tauri version

--- demo/src/main.js
+++ demo/src/main.js
@@ -1,4 +1,4 @@
-const { invoke } = window.__TAURI__.tauri;
+const { invoke } = window.__TAURI__.core;
 
 window.addEventListener("DOMContentLoaded", () => {
   document.querySelector("#test-1").addEventListener("click", async () => {
